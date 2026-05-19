55195cb9a555ec57bf03652208fb1611c7d1dd84
ozlemakcura <ozlemakcura7@gmail.com>
2026-05-19 23:05:08 +0300

    Touch button helper

--- button-fix.js
+++ button-fix.js
@@ -2,5 +2,5 @@
   setInterval(() => {
     const button = document.querySelector('#cat-form button[type="submit"]');
     if (button) button.removeAttribute('disabled');
-  }, 250);
+  }, 200);
 })();
